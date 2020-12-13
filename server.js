e701b3902cd01dfde70a53c1bd51d7eacecd9c40
Efrain Nevarez <efrain.nevarezjr@gmail.com>
2020-12-13 10:22:34 -0600

    addded listening port to server js

--- server.js
+++ server.js
@@ -12,3 +12,5 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
